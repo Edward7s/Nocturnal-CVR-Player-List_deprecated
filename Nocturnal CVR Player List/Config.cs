5adda7444e38ca72b72b255fca0ede2ea2267873
Edward7 <ediro174@gmail.com>
2022-08-10 12:54:15 +0300

    Fixed For BepInEx

--- Nocturnal CVR Player List/Config.cs
+++ Nocturnal CVR Player List/Config.cs
@@ -14,31 +14,33 @@ namespace Nocturnal
     {
         public string Background { get; set; }
 
+        public bool LeftSide { get; set; }
     }
     internal class Config
     {
         public Json Js { get; set; }
         public static Config Instance { get; set; }
-      
+
         public Config()
         {
             Instance = this;
             if (!Directory.Exists(Directory.GetCurrentDirectory() + "//Nocturnal"))
                 Directory.CreateDirectory(Directory.GetCurrentDirectory() + "//Nocturnal");
 
-            if (!File.Exists(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfig.Json"))
+            if (!File.Exists(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfigV1.1.Json"))
             {
                 using (WebClient wc = new WebClient())
                 {
-                    File.WriteAllText(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfig.Json", JsonConvert.SerializeObject(new Json()
+                    File.WriteAllText(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfigV1.1.Json", JsonConvert.SerializeObject(new Json()
                     {
                         Background = Convert.ToBase64String(wc.DownloadData("https://raw.githubusercontent.com/Edward7s/Nocturnal-CVR-Player-List/master/Nocturnal%20CVR%20Player%20List/Images/mask.jpg.png?token=GHSAT0AAAAAABR34D7BGOY4LIKQNVV7HPRMYXG34UQ")),
+                        LeftSide = true
                     }));
 
-                 wc.Dispose();
+                    wc.Dispose();
                 }
             }
-            Js = JsonConvert.DeserializeObject<Json>(File.ReadAllText(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfig.Json"));
+            Js = JsonConvert.DeserializeObject<Json>(File.ReadAllText(Directory.GetCurrentDirectory() + "//Nocturnal//PlayerListConfigV1.1.Json"));
         }
     }
 }
